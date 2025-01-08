34d3dac1a86b3a8753721bc1ad2657889b7d0fcf
Sameer D'Costa <sameerdcosta@gmail.com>
2025-01-08 11:39:07 -0600

    Testing seq_io package
    ```shell
    cat data/small.fasta | ./target/debug/subsample-fasta
    ```

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+
+
 fn main() {
-    println!("Hello, world!");
+  use seq_io::fasta::{Reader,Record};
+  use std::io;
+  
+  let mut reader = Reader::new(io::stdin());
+  let mut stdout = io::stdout();
+  
+  while let Some(result) = reader.next() {
+      let record = result.unwrap();
+      // determine sequence length
+      let seqlen = record.seq_lines()
+                         .fold(0, |l, seq| l + seq.len());
+      if seqlen > 100 {
+          record.write_wrap(&mut stdout, 80).unwrap();
+      } else {
+          eprintln!("{} is only {} long", record.id().unwrap(), seqlen);
+      }
+  }
 }
