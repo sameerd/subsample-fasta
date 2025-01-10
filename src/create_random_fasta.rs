c65370765aa1d1b8322918ddf30926ae4144f0d8
Sameer D'Costa <dcosta2@Sameers-M4-macbook-pro.local>
2025-01-10 16:23:37 -0600

    Adding command line arguments and changing some defaults to print wrapped fasta

--- src/create_random_fasta.rs
+++ src/create_random_fasta.rs
@@ -6,6 +6,28 @@ use rand_pcg::Pcg64;
 
 use seq_io::fasta::{Record, OwnedRecord};
 
+use clap::{Parser};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+
+  #[arg(short, long, default_value_t = 10)]
+  seed: u64,
+
+  #[arg(short, long, default_value_t = 10)]
+  num_seq: u64,
+
+  #[arg(long, default_value_t = 50)]
+  min_size: usize,
+
+  #[arg(long, default_value_t = 500)]
+  max_size: usize,
+
+  #[arg(short, long, default_value_t = 80)]
+  wrap_size: usize,
+
+}
 
 fn generate_random_aa(rng : &mut impl Rng, len: usize) -> Vec<u8> {
   // Copied from https://stackoverflow.com/a/74953997
@@ -15,19 +37,23 @@ fn generate_random_aa(rng : &mut impl Rng, len: usize) -> Vec<u8> {
   iter::repeat_with(one_u8).take(len).collect()
 }
 
+
 fn main() {
 
+  let cli = Cli::parse();
   let stdout = io::stdout();
-  let mut rng = Pcg64::seed_from_u64(10); 
-
-  let n: u16 = 10; // number of random sequences
+  let mut rng = Pcg64::seed_from_u64(cli.seed); 
+  let n = cli.num_seq; // number of random sequences
+  let min_size = cli.min_size;
+  let max_size = cli.max_size;
+  let wrap_size = cli.wrap_size;
 
   for i in 0..n {
-    let rec_head = [b"Random_", i.to_string().as_bytes()].concat();
-    let seq_len : usize = rng.gen_range(20..50);
+    let rec_head = [b"Random_", {i+1}.to_string().as_bytes()].concat();
+    let seq_len : usize = rng.gen_range(min_size..max_size);
     let rec_seq = generate_random_aa(&mut rng, seq_len);
     let rec = OwnedRecord{head: rec_head, seq: rec_seq};
-    _ = rec.write(&stdout);
+    _ = rec.write_wrap(&stdout, wrap_size);
   }
 
 
