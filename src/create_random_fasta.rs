d915e484d821c2f08c98f550f3c571c7a44f97c5
sameerdcosta <sameerdcosta@imac.lan>
2025-01-09 21:42:46 -0600

    Using seq_io write functions

--- src/create_random_fasta.rs
+++ src/create_random_fasta.rs
@@ -1,30 +1,33 @@
 use std::iter;
+use std::io;
 
 use rand::prelude::*;
-
 use rand_pcg::Pcg64;
 
-// use seq_io::fasta::{Reader,Record};
+use seq_io::fasta::{Record, OwnedRecord};
 
-fn generate_random_aa(rng : &mut impl Rng, len: usize) -> String {
+
+fn generate_random_aa(rng : &mut impl Rng, len: usize) -> Vec<u8> {
   // Copied from https://stackoverflow.com/a/74953997
+  // Changed to return Vec<u8>
   const STANDARD_AA: &[u8] = b"ACDEFGHIKLMNPQRSTVWY";
-  //let mut rng = rand::thread_rng();
-  let one_char = || STANDARD_AA[rng.gen_range(0..STANDARD_AA.len())] as char;
-  iter::repeat_with(one_char).take(len).collect()
+  let one_u8 = || STANDARD_AA[rng.gen_range(0..STANDARD_AA.len())] ;
+  iter::repeat_with(one_u8).take(len).collect()
 }
 
 fn main() {
 
+  let stdout = io::stdout();
   let mut rng = Pcg64::seed_from_u64(10); 
 
   let n: u16 = 10; // number of random sequences
 
   for i in 0..n {
-    let seq_id = format!( "Random_{}", i);
+    let rec_head = [b"Random_", i.to_string().as_bytes()].concat();
     let seq_len : usize = rng.gen_range(20..50);
-    let random_seq = generate_random_aa(&mut rng, seq_len);
-    println!(">{}\n{}", seq_id, random_seq);
+    let rec_seq = generate_random_aa(&mut rng, seq_len);
+    let rec = OwnedRecord{head: rec_head, seq: rec_seq};
+    _ = rec.write(&stdout);
   }
 
 
